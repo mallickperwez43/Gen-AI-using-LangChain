3fde045aff23da592b64490a2c1874e023cd7c6e
Perwez Mallick <mallickperwez43@gmail.com>
2026-09-06 21:54:20 +0530

    Structured Output using Standard Schema

---
+++ 03-LangChain-Structured-Output/src/02-structured-output-standard-schema/structured-output-standard-schema.ts
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import { ChatGroq } from "@langchain/groq";
+import * as v from "valibot";
+import { toStandardJsonSchema } from "@valibot/to-json-schema";
+
+const GROQ_KEY = process.env.GROQ_API_KEY;
+
+if (!GROQ_KEY) {
+    throw new Error("No GROQ API key found");
+}
+
+const model = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature: 0.1,
+    apiKey: GROQ_KEY,
+});
+
+const RawReviewSchema = v.object({
+    key_themes: v.pipe(v.array(v.string()), v.description("Write down all the key themes discussed in the review in an array")),
+    summary: v.pipe(v.string(), v.description("A brief summary of the review")),
+    sentiment: v.pipe(v.picklist(["pos", "neg"]), v.description("Return sentiment of the review either negative, positive or neutral")),
+    pros: v.optional(v.pipe(v.array(v.string()), v.description("Write down all the pros inside a list"))),
+    cons: v.optional(v.pipe(v.array(v.string()), v.description("Write down all the cons inside a list"))),
+    name: v.optional(v.pipe(v.string(), v.description("Write the name of the reviewer"))),
+});
+
+type ReviewType = v.InferOutput<typeof RawReviewSchema>;
+
+const ReviewSchema = toStandardJsonSchema(RawReviewSchema);
+
+const structuredModel = model.withStructuredOutput(ReviewSchema);
+
+const result = await structuredModel.invoke(`
+I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say,
+it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes
+everything lightning fast—whether I’m gaming, multitasking, or editing
+photos. The 5000mAh battery easily lasts a full day even with heavy use,
+and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches,
+though I don't use it often. What really blew me away is the 200MP camera—
+the night mode is stunning, capturing crisp, vibrant images even in low
+light. Zooming up to 100x actually works well for distant objects, but
+anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed
+use. Also, Samsung’s One UI still comes with bloatware—why do I need five
+different Samsung apps for things Google already provides? The $1,300 price
+tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+Review by Alex
+`) as ReviewType;
+
+console.log(result);
+console.log("Reviewer's Name: ", result.name);
+console.log("Reviewer's Sentiment: ", result.sentiment);
